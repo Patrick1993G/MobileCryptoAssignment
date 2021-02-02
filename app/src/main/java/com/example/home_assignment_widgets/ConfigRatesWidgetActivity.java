bc52ada3f068f1ed8a19514c13e07599014d239d
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2021-02-02 19:03:33 +0100

    first commit

--- app/src/main/java/com/example/home_assignment_widgets/ConfigRatesWidgetActivity.java
+++ app/src/main/java/com/example/home_assignment_widgets/ConfigRatesWidgetActivity.java
@@ -2,13 +2,75 @@ package com.example.home_assignment_widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RemoteViews;
 
-public class DownloadedRatesWidgetActivity extends AppCompatActivity {
+public class ConfigRatesWidgetActivity extends AppCompatActivity {
+
+    public static final String SHARE_PREFS = "widget_config";
+    public static final String PREF_BITCOIN = "bitcoin_config";
+    public static final String PREF_CRO = "cro_config";
+    public static final String PREF_ETH = "etherium_config";
+    public static final String PREF_MOB = "mob_config";
+    private EditText txtMob,txtBitcoin,txtCRO,txtEtherium;
+    private int widgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates_widget);
+
+        Intent configIntent = getIntent();
+        Bundle extras = configIntent.getExtras();
+
+        if(extras != null){
+            widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        Intent result = new Intent();
+        result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,widgetId);
+        setResult(RESULT_CANCELED, result);
+
+        if(widgetId == AppWidgetManager.INVALID_APPWIDGET_ID){
+            finish();
+        }
+
+        txtBitcoin = findViewById(R.id.editTextBitcoin);
+        txtCRO = findViewById(R.id.editTextCRO);
+        txtEtherium = findViewById(R.id.editTextEtherium);
+        txtMob = findViewById(R.id.editTextMob);
+    }
+    public void saveWidgetConfig(View v){
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        String bitcoin =String.valueOf(txtBitcoin.getText());
+        String cro =String.valueOf(txtCRO.getText());
+        String eth =String.valueOf(txtEtherium.getText());
+        String mob =String.valueOf(txtMob.getText());
+
+        RemoteViews remoteViews = new RemoteViews(this.getPackageName(), R.layout.crypto_widget);
+        remoteViews.setOnClickPendingIntent(R.id.btnConfig, pendingIntent);
+
+        appWidgetManager.updateAppWidget(widgetId,remoteViews);
+
+        SharedPreferences preferences = getSharedPreferences(SHARE_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(PREF_BITCOIN+widgetId,bitcoin);
+        editor.putString(PREF_CRO+widgetId, cro);
+        editor.putString(PREF_ETH+widgetId,eth);
+        editor.putString(PREF_MOB+widgetId, mob);
+        editor.apply();
+
+        Intent result = new Intent();
+        result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,widgetId);
+        setResult(RESULT_OK, result);
+        finish();
     }
 }
--- app/src/main/java/com/example/home_assignment_widgets/CryptoRatesApp.java
+++ app/src/main/java/com/example/home_assignment_widgets/CryptoRatesApp.java
@@ -5,19 +5,34 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import static com.example.home_assignment_widgets.ConfigRatesWidgetActivity.PREF_BITCOIN;
+import static com.example.home_assignment_widgets.ConfigRatesWidgetActivity.PREF_CRO;
+import static com.example.home_assignment_widgets.ConfigRatesWidgetActivity.PREF_ETH;
+import static com.example.home_assignment_widgets.ConfigRatesWidgetActivity.PREF_MOB;
+import static com.example.home_assignment_widgets.ConfigRatesWidgetActivity.SHARE_PREFS;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class CryptoRatesApp extends AppWidgetProvider {
 
+    public static String mob;
+    public static int bitcoin,eth,cro;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         //set intent
         Intent intent = new Intent(context,MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
 
+        //load from shared preferences
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARE_PREFS, Context.MODE_PRIVATE);
+        mob = sharedPreferences.getString(PREF_MOB+appWidgetId,"0000000");
+        bitcoin = Integer.parseInt(sharedPreferences.getString(PREF_BITCOIN+appWidgetId, "0"));
+        eth = Integer.parseInt(sharedPreferences.getString(PREF_ETH+appWidgetId, "0"));
+        cro = Integer.parseInt(sharedPreferences.getString(PREF_CRO+appWidgetId, "0"));
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.crypto_widget);
 
