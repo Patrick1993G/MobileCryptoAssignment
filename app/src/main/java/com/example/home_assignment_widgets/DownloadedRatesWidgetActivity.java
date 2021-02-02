d6b73dfeb09e654e4d7bb51dc8e3653de1f85493
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2021-02-02 15:25:13 +0100

    first commit

---
+++ app/src/main/java/com/example/home_assignment_widgets/DownloadedRatesWidgetActivity.java
@@ -0,0 +1,14 @@
+package com.example.home_assignment_widgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DownloadedRatesWidgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rates_widget);
+    }
+}
